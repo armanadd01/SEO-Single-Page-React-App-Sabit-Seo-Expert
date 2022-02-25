f6d78cb3451118cf78594abbc07e5601821de020
Arman Habib <arman@seoprotected.com>
2022-02-25 16:36:22 +0600

    responsive issues and anchor issues fixing

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Typical from "react-typical";
 import { Container, Row, Card, Col, ListGroup, ListGroupItem } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCopyright, faDownload, faLink, faMailBulk } from '@fortawesome/free-solid-svg-icons';
-import { faFacebook, faLinkedin, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faInstagram, faLinkedin, faTwitter, faWhatsapp, faYoutube } from '@fortawesome/free-brands-svg-icons';
 // import crunchbase from './Images/crunchbase-icon.svg'
 function App() {
   return (
     <div className="App">
       <Container>
         <Row>
-          <Col md={6}>
+          <Col xl={6} lg={12} md={12} sm={12}>
             <Card style={{ width: '90%' }} className='Card-design'>
               <Card.Img variant="top" style={{
                 height: '150px',
@@ -20,7 +20,6 @@ function App() {
                 borderRadius: '50%',
                 border: '1px solid #FFB530',
                 margin: '20px auto !important',
-
               }} src={image} alt="Shabit Ahtasham Songi" />
               <Card.Body>
                 <Card.Title > <h1> Shabit Ahtasham Songi </h1> </Card.Title>
@@ -37,40 +36,64 @@ function App() {
                     ]}
                     loop={Infinity}
                     wrapper="h6"
-
                   />
                 </Card.Title>
                 <br />
                 <Card.Text>
-                  <a href="https://www.seoguru.com.bd/ahtashamsongi/" target='_blank' className="text-decoration-none">Shabit Ahtasham Songi</a>  is an Internet Entrepreneur and SEO Expert based in Bangladesh, And the Founder & CEO of Multiple Online Business including SEO GURU a Local SEO and Digital Marketing Agency, Marketer's Panel a White Label SEO Agency, and SEO GURU Academy a Lead SEO Training Center where he helps to Learn SEO Practically.
+                  <a href="https://www.seoguru.com.bd/ahtashamsongi/" 
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="text-decoration-none">Shabit Ahtasham Songi</a>  is an Internet Entrepreneur and SEO Expert based in Bangladesh, And the Founder & CEO of Multiple Online Business including SEO GURU a Local SEO and Digital Marketing Agency, Marketer's Panel a White Label SEO Agency, and SEO GURU Academy a Lead SEO Training Center where he helps to Learn SEO Practically.
                   <br /><br />
                   Shabit Ahtasham Songi Doesn’t Just Rank Websites, He Grows Businesses Too. He is a self-depended guy who found success online one step at a time through Long Hours, Dedication, and a Good Sense of Humor along the way
                 </Card.Text>
               </Card.Body>
               <ListGroup className="list-group-flush">
                 <ListGroupItem>
-                  <a href="https://www.facebook.com/AhtashamSongi"><FontAwesomeIcon icon={faFacebook} /></a>
-                  <a href="https://bd.linkedin.com/in/ahtashamsongi"><FontAwesomeIcon icon={faLinkedin} /></a>
-                  <a href="https://www.crunchbase.com/person/shabit-ahtasham-songi"><FontAwesomeIcon icon={faLink} /></a>
-                  <a href="https://wa.me/+8801703928204"><FontAwesomeIcon icon={faWhatsapp} /></a>
+                  <a href="https://www.facebook.com/AhtashamSongi" 
+                      rel="noopener noreferrer"
+                      target="_blank"><FontAwesomeIcon icon={faFacebook} /></a>
+                  <a href="https://twitter.com/ahtashamsongi" 
+                      rel="noopener noreferrer"
+                      target="_blank"><FontAwesomeIcon icon={faTwitter} /></a>
+                  <a href="https://bd.linkedin.com/in/ahtashamsongi" 
+                      rel="noopener noreferrer"
+                      target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a>
+                  <a href="https://www.youtube.com/channel/UCTxV2V1dsSJM_WzUIoMI3nQ/videos" 
+                      rel="noopener noreferrer"
+                      target="_blank"><FontAwesomeIcon icon={faYoutube} /></a>
+                  <a href="https://www.pinterest.com/ahtashamsongi/" 
+                      rel="noopener noreferrer"
+                      target="_blank"><FontAwesomeIcon icon={faTwitter} /></a>
+                  <a href="https://www.instagram.com/shabitahtashamsongi/" 
+                      rel="noopener noreferrer"
+                      target="_blank"><FontAwesomeIcon icon={faInstagram} /></a>
+                  <a href="https://wa.me/+8801703928204" 
+                      rel="noopener noreferrer"
+                      target="_blank"><FontAwesomeIcon icon={faWhatsapp} /></a>
                 </ListGroupItem>
               </ListGroup>
               <Card.Body className='d-md-flex justify-content-center'>
                 <div className="d-md-flex flex-column justify-content-center">
-                <Card.Link href="https://www.shabitahtashamsongi.com/cv/Shabit-Ahtasham-Songi-Work-Resume.pdf" className='custom-block'><FontAwesomeIcon icon={faDownload} /> Download CV</Card.Link>
+                <Card.Link href="https://www.shabitahtashamsongi.com/cv/Shabit-Ahtasham-Songi-Work-Resume.pdf" className='custom-block' 
+                      rel="noopener noreferrer"
+                      target="_blank"><FontAwesomeIcon icon={faDownload} /> Download CV</Card.Link>
                 <Card.Link href="mailto:songi.clients@gmail.com" className='custom-block'><FontAwesomeIcon icon={faMailBulk} /> Email Me</Card.Link>
                 </div>
                 
               </Card.Body>
             </Card>
           </Col>
-          <Col md={6}>
+          <Col xl={6} lg={12} md={12} sm={12}>
             <div className="main-services">
               <Row>
                 <h3 className='heading-title'>Registered Online Business</h3>
                 <Col md={6} sm={12} className='d-sm-flex justify-content-center'>
                   <div className="Service-Box-One">
-                    <a href="https://www.seoguru.com.bd/" className="text-decoration-none">
+                    <a href="https://www.seoguru.com.bd/" 
+                      rel="noopener noreferrer"
+                      target="_blank"
+                      className="text-decoration-none">
                     <div class="serviceBox orange">
                       <h3 class="title"> SEO GURU</h3>
                     </div>
@@ -79,7 +102,10 @@ function App() {
                 </Col>
                 <Col md={6} sm={12} className='d-sm-flex justify-content-center'>
                   <div className="Service-Box-twos">
-                    <a href="https://training.seoguru.com.bd/" className="text-decoration-none">  
+                    <a href="https://training.seoguru.com.bd/"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                      className="text-decoration-none" >  
                     <div class="serviceBox orange">
                         
                         <h3 class="title">SEO GURU <br /> Academy</h3>
@@ -89,7 +115,10 @@ function App() {
                 </Col>
                 <Col md={12} sm={12} className='d-sm-flex justify-content-center'>
                   <div className="Service-Box-three">
-                    <a  href="https://marketerspanel.com/" className="text-decoration-none">
+                    <a  href="https://marketerspanel.com/" 
+                      rel="noopener noreferrer"
+                      target="_blank"
+                      className="text-decoration-none">
                       <div class="serviceBox orange">
                       <h3 class="title">Marketer's Panel</h3>
                       </div>
@@ -98,7 +127,6 @@ function App() {
                 </Col>
               </Row>
             </div>
-
           </Col>
         </Row>
       </Container>
