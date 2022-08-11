83ba8bbbb035536dee7107131fd96d6fc366d0b1
Arman Habib <arman@seoprotected.com>
2022-08-11 09:49:33 +0600

    new commit

---
+++ src/Components/About/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+
+const About = () => {
+    return (
+        <div>
+            <Container>
+                <div
+                    className='heading-title text-center'>
+                    <h1 className='text-light'>WHO IS <br /> Shabit Ahtesham Shongi</h1>
+
+                </div>
+                <Row>
+
+                </Row>
+                <Row>
+
+                </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default About;
