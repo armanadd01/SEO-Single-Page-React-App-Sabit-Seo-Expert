947b62f249a2f6c7d153d7cc812f15f0299ca4cd
Arman Habib <arman@seoprotected.com>
2022-08-22 18:04:12 +0600

    22-08-2022 contact form fixing

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -1,18 +1,28 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useRef } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
+import emailjs from '@emailjs/browser';
 
 const Contacts = () => {
     const { register, handleSubmit, reset } = useForm();
+    const form = useRef();
     //handle plans from
     const onSubmit = data => {
-        console.log(data)
-        axios.post('https://thawing-meadow-71532.herokuapp.com/mobiles', data)
+
+        axios.post('http://localhost:5000/contacts', data)
             .then(res => {
                 if (res.data.insertedId) {
-                    alert('Query Submitted Successfully');
+
+                    emailjs.sendForm('service_4vucu9f', 'template_1b8gj38', form.current, 'WwMM6HUPeHrDNxG_W')
+                        .then((result) => {
+                            console.log(form.current);
+                            console.log(result.text);
+                        }, (error) => {
+                            console.log(error.text);
+                        });
                     reset();
+                    alert('Query Submitted Successfully');
                 }
 
             })
@@ -25,15 +35,16 @@ const Contacts = () => {
                         <div className="form-section text-center test-light">
                             <h1 className="text-light">Submit Your Query </h1>
                             <div className="form">
-                                <form className="p-2" onSubmit={handleSubmit(onSubmit)}>
+                                <form ref={form} className="p-2" onSubmit={handleSubmit(onSubmit)}>
                                     <input className="form-control m-3" placeholder="Enter Your Name" type="text"  {...register("name", { required: true, maxLength: 40 })} />
                                     <input className="form-control m-3" placeholder="Enter Email" type="text"  {...register("email", { required: true })} />
                                     <input className="form-control m-3" placeholder="Enter Phone Number" type="text"  {...register("phone", { required: true })} />
                                     <input className="form-control m-3" placeholder="Enter website" type="text"  {...register("website")} />
                                     <input className="form-control m-3" placeholder="Enter Company Name" type="text"  {...register("company")} />
-                                    <input className="form-control m-3" placeholder="Enter Location" type="text"  {...register("Location")} />
+                                    <input className="form-control m-3" placeholder="Enter Location" type="text"  {...register("location")} />
                                     <textarea className="form-control m-3" placeholder="Enter Your Text"   {...register("text")} ></textarea>
-                                    <input className="btn btn-outline-light" type="submit" />
+                                    <input className="btn btn-outline-light" type="submit" value="Send" />
+
                                 </form>
                             </div>
                         </div>
